2260e842c166e109531bd0b05572a14848b147ea
Cassio Alves <cassiowin98@yahoo.com.br>
2016-05-06 11:13:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,21 +16,22 @@ app.get('/produto', function(req, res){
 	});										  
 });
 
-app.get('/estoque', function(req, res){ 
+app.get('/produto/:nome', function(req, res){ 
 
-	produtoController.list(function(resp) { 
+	var nome = req.params.nome;
+
+	produtoController.Produto(nome, function(resp) { 
 											 
 		res.json(resp);					      
 	});										  
 });
 
-app.get('/produto/:nome', function(req, res){ 
+app.post('/produto/pedido/:nome', function(req, res){ 
 
-	var nome = validator.trim(validator.escape(req.params.nome));
-	var quantidadeProdutos = validator.trim(validator.escape(req.param('quantidadeProdutos')));
-	var tamanho = validator.trim(validator.escape(req.param('tamanho')));
+	var nome = validator.trim(validator.escape(req.body.nome));
+	var tamanho = req.body.tamanho;
 
-	produtoController.user(nome, quantidadeProdutos, tamanho, function(resp){
+	produtoController.Pedido(nome, tamanho,function(resp){
 
 		res.json(resp);
 	});
@@ -42,23 +43,24 @@ app.post('/produto', function(req, res){
 	var nome = validator.trim(validator.escape(req.body.nome));
 	var preco = validator.trim(validator.escape(req.body.preco));
 	var descricao = validator.trim(validator.escape(req.body.descricao));
+	var cor = req.body.cor;
 	var tamanho = req.body.tamanho;
 
-	produtoController.save(nome, preco, descricao, tamanho, function(resp){
+	produtoController.save(nome, preco, descricao, cor,tamanho, function(resp){
 
 		res.json(resp);
 	});
 });
 
-app.put('/produto', function(req, res){ //atualiza um usuario
+app.put('/produto/:nome', function(req, res){
 
-	var id = validator.trim(validator.escape(req.param('id')));
-    var nome = validator.trim(validator.escape(req.param('nome')));
-	var preco = validator.trim(validator.escape(req.param('preco')));
-	var descricao = validator.trim(validator.escape(req.param('descricao')));
-	var tamanho = validator.trim(validator.escape(req.param('tamanho')));
+    var nome = validator.trim(validator.escape(req.params.nome));
+	var preco = validator.trim(validator.escape(req.body.preco));
+	var descricao = validator.trim(validator.escape(req.body.descricao));
+	var cor = req.body.cor;
+	var tamanho = req.body.tamanho;
 
-	produtoController.update(id, nome, preco, descricao, tamanho, function(resp){
+	produtoController.update(nome, preco, descricao, cor, tamanho, function(resp){
 
 		res.json(resp);
 	});
